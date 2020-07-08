c17db5a6da3ca0a24fcaa8a5f9f9c43460c9f01a
libin <civet148@126.com>
2020-07-08 19:16:34 +0800

    [sqlca] update count method usage comment

--- engine.go
+++ engine.go
@@ -388,6 +388,7 @@ func (e *Engine) Slave() *Engine {
 
 // orm count records
 // SELECT COUNT(*) FROM table WHERE ...
+// count, err := e.Model(nil).Table("users").Where("delete=1").Count()
 func (e *Engine) Count() (count int64, err error) {
 	e.setModel(&count)
 	e.setSelectColumns("COUNT(*)")

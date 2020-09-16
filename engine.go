c6004312413f2f1b78fcbd80febdece04220d633
libin <civet148@126.com>
2020-09-16 10:30:58 +0800

    [sqlca] panic when input nil

--- engine.go
+++ engine.go
@@ -395,7 +395,7 @@ func (e *Engine) Desc(strColumns ...string) *Engine {
 // `field_name` IN ('1','2',...)
 func (e *Engine) In(strColumn string, args ...interface{}) *Engine {
 	if len(args) == 0 {
-		e.panic("IN conditions can't be nil")
+		panic("IN conditions can't be nil")
 	}
 	v := condition{
 		ColumnName:   strColumn,
@@ -408,7 +408,7 @@ func (e *Engine) In(strColumn string, args ...interface{}) *Engine {
 // `field_name` NOT IN ('1','2',...)
 func (e *Engine) Not(strColumn string, args ...interface{}) *Engine {
 	if len(args) == 0 {
-		e.panic("NOT IN conditions can't be nil")
+		panic("NOT IN conditions can't be nil")
 	}
 	v := condition{
 		ColumnName:   strColumn,

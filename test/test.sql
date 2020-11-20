76da7b2bbcd126989b725c139731d29254b25727
libin <civet148@126.com>
2020-11-20 10:25:50 +0800

    [sqlca] update README.md and test.sql

--- test/test.sql
+++ test/test.sql
@@ -11,8 +11,6 @@ MySQL - 8.0.18 : Database - test
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`test` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-
 USE `test`;
 
 /*Table structure for table `classes` */
@@ -26,35 +24,70 @@ CREATE TABLE `classes` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-/*Data for the table `classes` */
+/*Table structure for table `jsons` */
 
-insert  into `classes`(`id`,`class_no`,`user_id`,`created_at`,`updated_at`) values (1,'S-01',1,'2020-04-10 10:08:08','2020-05-12 19:39:43');
-insert  into `classes`(`id`,`class_no`,`user_id`,`created_at`,`updated_at`) values (2,'S-01',2,'2020-04-10 10:08:08','2020-05-12 19:39:44');
-insert  into `classes`(`id`,`class_no`,`user_id`,`created_at`,`updated_at`) values (3,'S-01',3,'2020-04-10 10:08:08','2020-05-12 19:39:45');
-insert  into `classes`(`id`,`class_no`,`user_id`,`created_at`,`updated_at`) values (4,'S-01',4,'2020-04-10 10:08:08','2020-05-12 19:39:45');
-insert  into `classes`(`id`,`class_no`,`user_id`,`created_at`,`updated_at`) values (5,'S-02',5,'2020-04-10 10:08:08','2020-05-12 19:39:46');
-insert  into `classes`(`id`,`class_no`,`user_id`,`created_at`,`updated_at`) values (6,'S-02',8,'2020-04-10 10:08:08','2020-05-12 19:40:00');
-insert  into `classes`(`id`,`class_no`,`user_id`,`created_at`,`updated_at`) values (7,'S-02',9,'2020-04-10 10:08:08','2020-04-10 10:08:08');
-insert  into `classes`(`id`,`class_no`,`user_id`,`created_at`,`updated_at`) values (8,'S-02',10,'2020-04-10 10:08:08','2020-04-10 10:08:08');
+CREATE TABLE `jsons` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增ID',
+  `name` varchar(16) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '用户名',
+  `sex` int(11) NOT NULL DEFAULT '1' COMMENT '性别',
+  `user_data` json NOT NULL COMMENT '用户JSON数据',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Table structure for table `orders` */
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增ID(主键)',
+  `details` json DEFAULT NULL COMMENT '订单明细',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `location` point DEFAULT NULL COMMENT '位置',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Table structure for table `t_address` */
+
+CREATE TABLE `t_address` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `lng` double(11,7) DEFAULT NULL,
+  `lat` double(11,7) DEFAULT NULL,
+  `name` char(80) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `geohash` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `location` point DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Table structure for table `t_point` */
+
+CREATE TABLE `t_point` (
+  `addres_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `address_point` point NOT NULL,
+  `lng` double(11,7) DEFAULT NULL,
+  `lat` double(11,7) DEFAULT NULL,
+  `name` char(80) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `geohash` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`addres_id`),
+  SPATIAL KEY `address_point` (`address_point`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Table structure for table `users` */
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'auto inc id',
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'auto inc id',
   `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT 'user name',
-  `phone` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'phone number',
-  `sex` tinyint(1) NOT NULL COMMENT 'user sex',
+  `phone` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT 'phone number',
+  `sex` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT 'user sex',
   `email` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT 'email',
   `disable` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'disabled(0=false 1=true)',
   `balance` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT 'balance of decimal',
-  `sex_name` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT 'sex name',
+  `sex_name` varchar(16) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT 'sex name',
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'create time',
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'update time',
   PRIMARY KEY (`id`) USING BTREE,
   KEY `phone` (`phone`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
-
-/*Data for the table `users` */
+) ENGINE=InnoDB AUTO_INCREMENT=372 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;

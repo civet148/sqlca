26d7fc1777b471de773f7fbca2258eceefd839fe
libin <libin@126.com>
2025-12-02 17:21:42 +0800

    distribution lock with redis

--- go.mod
+++ go.mod
@@ -1,12 +1,15 @@
 module github.com/civet148/sqlca/v3
 
-go 1.20
+go 1.22
+
+toolchain go1.22.12
 
 require (
 	gitee.com/opengauss/openGauss-connector-go-pq v1.0.7
 	github.com/bwmarrin/snowflake v0.3.0
 	github.com/civet148/gotools v1.4.1
 	github.com/civet148/log v1.7.2
+	github.com/civet148/redigo v0.1.0
 	github.com/denisenkom/go-mssqldb v0.0.0-20200206145737-bbfc9a55622e
 	github.com/elliotchance/sshtunnel v1.2.0
 	github.com/gansidui/geohash v0.0.0-20141019080235-ebe5ba447f34
@@ -21,6 +24,7 @@ require (
 require (
 	github.com/fatih/color v1.12.0 // indirect
 	github.com/golang-sql/civil v0.0.0-20190719163853-cb61b32ac6fe // indirect
+	github.com/gomodule/redigo v1.9.2 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/tjfoc/gmsm v1.4.1 // indirect
--- options.go
+++ options.go
@@ -1,17 +1,37 @@
 package sqlca
 
 import (
+	"crypto/tls"
 	"github.com/civet148/log"
+	"time"
 )
 
+type RedisConfig struct {
+	Address     string         // redis address, eg. "127.0.0.1:6379"
+	Password    string         // redis password, default empty
+	DB          int            // db index, default 0
+	MaxActive   int            // max active connections
+	MaxIdle     int            // max idle connections
+	ConnTimeout *time.Duration // connection timeout
+
+	// Close connections older than this duration. If the value is zero, then
+	// the pool does not close connections based on age.
+	MaxConnLifetime time.Duration
+	ClientName      string
+	UseTLS          bool
+	SkipVerify      bool
+	TlsConfig       *tls.Config
+}
+
 type dialOption struct {
-	Debug         bool       //enable debug mode
-	Max           int        //max active connections
-	Idle          int        //max idle connections
-	SSH           *SSH       //ssh tunnel server config
-	SnowFlake     *SnowFlake //snowflake id config
-	DisableOffset bool       //disable page offset for LIMIT (default page no is 1, if true then page no start from 0)
-	DefaultLimit  int32      //limit default (0 means no limit)
+	Debug         bool         //enable debug mode
+	Max           int          //max active connections
+	Idle          int          //max idle connections
+	SSH           *SSH         //ssh tunnel server config
+	SnowFlake     *SnowFlake   //snowflake id config
+	DisableOffset bool         //disable page offset for LIMIT (default page no is 1, if true then page no start from 0)
+	DefaultLimit  int32        //limit default (0 means no limit)
+	RedisConfig   *RedisConfig //redis config
 }
 
 type Option func(*dialOption)
@@ -21,7 +41,7 @@ var defaultDialOption = &dialOption{
 	Idle: DefaultConnIdle,
 }
 
-func makeOption(opts ...Option) *dialOption {
+func parseDialOption(opts ...Option) *dialOption {
 	for _, opt := range opts {
 		opt(defaultDialOption)
 	}
@@ -68,5 +88,11 @@ func WithSSH(ssh *SSH) Option {
 func WithSnowFlake(snowflake *SnowFlake) Option {
 	return func(opt *dialOption) {
 		opt.SnowFlake = snowflake
+	}
+}
+
+func WithRedisConfig(rc *RedisConfig) Option {
+	return func(opt *dialOption) {
+		opt.RedisConfig = rc
 	}
 }

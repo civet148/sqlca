5178d4bebb3aa9f816bdedf09bc04ea421a19756
libin <libin@126.com>
2026-08-29 17:25:55 +0800

    update models with db2go v3.10.9

--- example/gen_models.sh
+++ example/gen_models.sh
@@ -7,7 +7,7 @@ PACK_NAME="models"
 # 只读字段(不更新)
 READ_ONLY=""
 # 指定表名(不指定则整个数据库全部导出,排除某个表则在前面加-符号)
-TABLE_NAME="-users"
+TABLE_NAME=""
 # 忽略字段名(逗号分隔)
 WITH_OUT="created_at,updated_at"
 # 添加标签
--- example/models/users.go
+++ example/models/users.go
@@ -1,13 +1,19 @@
 package models
 
 import (
-	"time"
-
 	"github.com/civet148/log"
 	"github.com/civet148/sqlca/v3"
 )
 
-const TableNameUsers = "users" //
+const TableNameUsers = "users"
+
+const (
+	UsersColumn_Id        = "id"
+	UsersColumn_UserName  = "user_name"
+	UsersColumn_State     = "state"
+	UsersColumn_Email     = "email"
+	UsersColumn_ExtraData = "extra_data"
+)
 
 const (
 	USERS_COLUMN_ID         = "id"
@@ -18,32 +24,42 @@ const (
 )
 
 type User struct {
-	BaseModel
-	Id       uint64      `json:"id,omitempty" db:"id" gorm:"column:id;primaryKey;autoIncrement;"`                                                             //
-	UserName string      `json:"user_name,omitempty" db:"user_name" gorm:"column:user_name;type:varchar(32);uniqueIndex:idx_users_user_name;" sqlca:"isnull"` //
-	Email    string      `json:"email,omitempty" db:"email" gorm:"column:email;type:varchar(64);uniqueIndex:idx_users_email;" sqlca:"isnull"`                 //
-	Roles    []*Role     `json:"roles,omitempty" db:"-" gorm:"many2many:user_roles;"`                                                                         // 用户角色列表
-	Profile  UserProfile `json:"profile,omitempty" db:"-" gorm:"foreignKey:UserId;"`                                                                          // 用户资料明细
+	Id        uint64      `json:"id" db:"id" gorm:"column:id;primaryKey;autoIncrement;"`
+	UserName  string      `json:"user_name" db:"user_name" gorm:"column:user_name;type:varchar(32);uniqueIndex:idx_users_user_name,priority:1;null;" sqlca:"nullable"`
+	State     int8        `json:"state" db:"state" gorm:"column:state;type:tinyint(1);default:0;null;" sqlca:"nullable"`
+	Email     string      `json:"email" db:"email" gorm:"column:email;type:varchar(64);uniqueIndex:idx_users_email,priority:1;null;" sqlca:"nullable"`
+	ExtraData struct{}    `json:"extra_data" db:"extra_data" gorm:"column:extra_data;type:json;null;" sqlca:"nullable"`
+	Roles     []*Role     `json:"roles,omitempty" db:"-" gorm:"many2many:user_roles;"` // 用户角色列表
+	Profile   UserProfile `json:"profile,omitempty" db:"-" gorm:"foreignKey:UserId;"`  // 用户资料明细
+	BaseModel `json:"-" db:"-" gorm:"embedded"`
 }
 
-func (do User) TableName() string { return "users" }
+func (do User) DatabaseName() string { return "test" }
 
-func (do User) GetId() uint64           { return do.Id }
-func (do User) GetCreatedAt() time.Time { return do.CreatedAt }
-func (do User) GetUpdatedAt() time.Time { return do.UpdatedAt }
-func (do User) GetUserName() string     { return do.UserName }
-func (do User) GetEmail() string        { return do.Email }
+func (do User) TableName() string { return TableNameUsers }
 
-func (do *User) SetId(v uint64)           { do.Id = v }
-func (do *User) SetCreatedAt(v time.Time) { do.CreatedAt = v }
-func (do *User) SetUpdatedAt(v time.Time) { do.UpdatedAt = v }
-func (do *User) SetUserName(v string)     { do.UserName = v }
-func (do *User) SetEmail(v string)        { do.Email = v }
+func (do User) GetId() uint64 { return do.Id }
 
-////////////////////// ----- 自定义代码请写在下面 ----- //////////////////////
+func (do User) GetUserName() string { return do.UserName }
+
+func (do User) GetState() int8 { return do.State }
+
+func (do User) GetEmail() string { return do.Email }
+
+func (do User) GetExtraData() struct{} { return do.ExtraData }
+
+func (do *User) SetId(v uint64) { do.Id = v }
+
+func (do *User) SetUserName(v string) { do.UserName = v }
+
+func (do *User) SetState(v int8) { do.State = v }
+
+func (do *User) SetEmail(v string) { do.Email = v }
 
 func (do *User) AfterQueryData(db *sqlca.Engine, ok bool) error {
 	log.Infof("query ok [%v]", ok)
 	do.isExist = true
 	return nil
 }
+
+func (do *User) SetExtraData(v struct{}) { do.ExtraData = v }
